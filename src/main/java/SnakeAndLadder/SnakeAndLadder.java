f16b04b4d29fcc1de53c7a4147608906cc9f3a26
adityagoel28 <adityagoel2002@gmail.com>
2023-12-20 19:18:42 +0530

    Dice roll using Random function

--- src/main/java/SnakeAndLadder/SnakeAndLadder.java
+++ src/main/java/SnakeAndLadder/SnakeAndLadder.java
@@ -4,6 +4,11 @@ import java.util.Random;
 
 public class SnakeAndLadder {
     private static int currentPlayer = 0;
+    private static Random random = new Random();
+
+    private static int rollDice() {
+        return random.nextInt(6) + 1; // Generates a random number between 1 and 6
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator!");
