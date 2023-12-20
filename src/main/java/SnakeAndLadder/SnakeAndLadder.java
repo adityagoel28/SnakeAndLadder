4365d2816fda641818f07d8484743d426c189c3c
adityagoel28 <adityagoel2002@gmail.com>
2023-12-20 19:12:45 +0530

    Initialized Snake and Ladder Simulator Program

---
+++ src/main/java/SnakeAndLadder/SnakeAndLadder.java
@@ -0,0 +1,9 @@
+package SnakeAndLadder;
+
+import java.util.Random;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Simulator!");
+    }
+}
