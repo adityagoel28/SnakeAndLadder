3b5e9134fea0e11eedd51b1062a9eacec2fd5389
adityagoel28 <adityagoel2002@gmail.com>
2023-12-20 19:16:18 +0530

    Player starting at position 0

--- src/main/java/SnakeAndLadder/SnakeAndLadder.java
+++ src/main/java/SnakeAndLadder/SnakeAndLadder.java
@@ -3,7 +3,10 @@ package SnakeAndLadder;
 import java.util.Random;
 
 public class SnakeAndLadder {
+    private static int currentPlayer = 0;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Simulator!");
+        int diceRolls = 0;
     }
 }
